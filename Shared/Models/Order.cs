9bee3748c56af13598cfd82efe3e5f5ec0750667
Md Redwan <136357607+RedwanHossain96@users.noreply.github.com>
2023-10-10 22:13:37 +0600

    Add files via upload

---
+++ Shared/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExam.Shared.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public string? CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool IsActive { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
